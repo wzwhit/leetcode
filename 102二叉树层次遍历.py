6915811f4747fdc0b08d3220138ca2749efbf44f
wzwhit <1064951590@qq.com>
2019-04-19 11:38:30 +0800

    二叉树层次遍历

--- 102二叉树层次遍历.py
+++ 102二叉树层次遍历.py
@@ -6,16 +6,16 @@
 #         self.right = None
 #给定一个二叉树，返回其按层次遍历的节点值
 #即逐层地，从左到右访问所有节点
-#二叉树层次遍历
+#二叉树层次遍历,迭代
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if root == None:
             return []
-        currentstack = [root]
+        currentstack = [root]#当前行
         outlist = []
         while currentstack:
             res = []
-            nextstack = []
+            nextstack = []#顺序保存当前行的所有左右子节点
             for node in currentstack:
                 res.append(node.val)
                 if node.left:
@@ -23,5 +23,5 @@ class Solution:
                 if node.right:
                     nextstack.append(node.right)
             outlist.append(res)
-            currentstack = nextstack
+            currentstack = nextstack#切换下一行
         return outlist
