54d7ecf7bc9f91c12eb801395850de9bfcffdbe9
wzwhit <1064951590@qq.com>
2019-04-19 11:25:37 +0800

    二叉树迭代中序遍历为升序为有效

---
+++ 98验证二叉搜索树.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#  给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+#  假设一个二叉搜索树具有如下特征：
+    #  节点的左子树只包含小于当前节点的数。
+    #  节点的右子树只包含大于当前节点的数。
+    #  所有左子树和右子树自身必须也是二叉搜索树。
+###中序遍历，保证中序遍历结果是升序为有效，否则为无效
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        if root == None:
+            return True
+        if root.val == None:
+            return False
+        stack = []
+        out = []
+        while root != None or stack:
+            while root != None:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            out.append(root.val)
+            root = root.right
+        for i in range(len(out)-1):
+            if out[i] >= out[i+1]:
+                return False
+        return True
