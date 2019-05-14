0ca274ceb4c3a311c2778f8c9f7634d4ec93f148
wzwhit <1064951590@qq.com>
2019-05-14 14:23:41 +0800

    Hash Table

---
+++ 49字母异位词分组.py
@@ -0,0 +1,25 @@
+#  给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+#  示例:
+#  输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+#  输出:
+#  [
+  #  ["ate","eat","tea"],
+  #  ["nat","tan"],
+  #  ["bat"]
+#  ]
+#  说明：
+    #  所有输入均为小写字母。
+    #  不考虑答案输出的顺序。
+
+#Hash Table
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        k = {}
+        for s in strs:
+            ss = ''.join(sorted(s))
+            #print(ss)
+            if k.get(ss):
+                k[ss].append(s)
+            else:
+                k[ss] = [s]
+        return list(k.values())
