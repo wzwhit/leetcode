634f0aad4952254ba04640b363e0ccf614b47d4d
wzwhit <1064951590@qq.com>
2019-06-19 17:46:42 +0800

    字符串

--- 48旋转图像.py
+++ 48旋转图像.py
@@ -41,3 +41,12 @@ class Solution:
         for j in range(0,n//2):
             for i in range(n-1-2*j):
                 matrix[0+j][0+j+i], matrix[0+j+i][n-1-j], matrix[n-1-j][n-1-j-i], matrix[n-1-j-i][0+j] = matrix[n-1-j-i][0+j], matrix[0+j][0+j+i], matrix[0+j+i][n-1-j], matrix[n-1-j][n-1-j-i]
+#  顺时针旋转先转置再翻转每一行,若逆时针则先转置再翻转列
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        b = list(zip(*matrix))
+        for i in range(len(matrix)):
+            matrix[i] = list(b[i][::-1])
