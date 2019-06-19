06226aa162ae94f7381a46a084d189af1272d862
wzwhit <1064951590@qq.com>
2019-06-19 20:24:27 +0800

    字符串

--- 38报数.py
+++ 38报数.py
@@ -12,42 +12,42 @@
 # Testcase Example:  '1'
 #
 # 报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
-# 
+#
 # 1.     1
 # 2.     11
 # 3.     21
 # 4.     1211
 # 5.     111221
-# 
-# 
+#
+#
 # 1 被读作  "one 1"  ("一个一") , 即 11。
 # 11 被读作 "two 1s" ("两个一"）, 即 21。
 # 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
-# 
+#
 # 给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
-# 
+#
 # 注意：整数顺序将表示为一个字符串。
-# 
-# 
-# 
+#
+#
+#
 # 示例 1:
-# 
+#
 # 输入: 1
 # 输出: "1"
-# 
-# 
+#
+#
 # 示例 2:
-# 
+#
 # 输入: 4
 # 输出: "1211"
-# 
-# 
+#
+#
 #
 class Solution:
-    def countAndSay(self, n: 'int') -> 'str': 
+    def countAndSay(self, n: 'int') -> 'str':
         out = ['1']
         if n == 1:
-            return out[0]                  
+            return out[0]
         for i in range(0,n-1):
             m = len(out[i])
             s = ''
@@ -56,19 +56,32 @@ class Solution:
                 if j < m-2 and out[i][j] == out[i][j+1] == out[i][j+2]:
                     s = s + str(3) + str(out[i][j])
                     j += 2
-                    
                 elif j < m-1 and out[i][j] == out[i][j+1]:
                     s = s + str(2) + str(out[i][j])
                     j += 1
-                    
                 elif (j < m-1 and out[i][j] != out[i][j+1]) or j == m-1:
                     s = s + str(1) + str(out[i][j])
                 j += 1
                 if j == m:
                     break
-                    
-            #print(s)
             out.append(s)
         return out[n-1]
 
-        
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return '1'
+        say = ['' for i in range(31)]
+        say[0] = '1'
+        for i in range(1,n):
+            j = 0
+            while j < len(say[i-1]):
+                k = j+1
+                c = 1
+                while k < len(say[i-1]) and say[i-1][k] == say[i-1][j]:
+                    c += 1
+                    k += 1
+                say[i] += str(c) + say[i-1][j]
+                j = k
+        return say[n-1]
