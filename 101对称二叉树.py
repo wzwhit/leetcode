0d0862c8391d42fc88dad28020f71675bc7cf639
wzwhit <1064951590@qq.com>
2019-07-02 16:43:22 +0800

    二叉树递归先序中序遍历

--- 101对称二叉树.py
+++ 101对称二叉树.py
@@ -4,7 +4,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-#递归
+#递归,先序遍历
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
         if root:
@@ -21,3 +21,25 @@ class Solution:
             return self.isSameTree(p.left,q.right) and self.isSameTree(p.right,q.left)
         else:
             return False
+
+#  中序遍历
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        def helper(left, right):
+            if not left and not right:
+                return True
+            elif not left or not right:
+                return False
+            if helper(left.left, right.right):
+                if left.val != right.val:
+                    return False
+                return helper(left.right, right.left)
+            else:
+                return False
+        return helper(root.left, root.right)
