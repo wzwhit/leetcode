dc63d65e0d2c86136ad08717c534c45df50451eb
wzwhit <31851330+wzwhit@users.noreply.github.com>
2019-07-30 16:18:15 +0800

    链表

--- 21合并两个有序链表.py
+++ 21合并两个有序链表.py
@@ -48,4 +48,11 @@ class Solution:
         elif l2:
             head.next = l2
         return out.next
-        
+####递归        
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
